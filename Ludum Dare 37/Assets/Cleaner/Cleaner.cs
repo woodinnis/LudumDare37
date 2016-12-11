19cadc9f235455f42e6d6df2b110ecafb14bb06e
woodinnis <wood.innis@gmail.com>
2016-12-10 20:29:39 -0600

    First Commit
    2D version of LD37 - One Room
    Prototype of Ball Pit Simulator

---
+++ Ludum Dare 37/Assets/Cleaner/Cleaner.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cleaner : MonoBehaviour {
+
+    public float moveSpeed;
+    public float smoothTime;
+
+    private Vector3 velocity = Vector3.zero;
+    //  Variable declarations for movement
+    private float journeyLength;
+    private float distCovered;
+    private float startTime;
+    private Transform startPoint;
+    private float fracJourney;
+    private Vector3 newPosition;
+
+    // Use this for initialization
+    void Start () {
+
+        //  Variable definitions for movement
+        startTime = Time.time;
+        startPoint = transform;
+        newPosition = transform.position;
+        fracJourney = 0f;
+}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("Fire1"))
+        { 
+            journeyLength = Vector3.Distance(transform.position, Input.mousePosition);
+
+            distCovered = (Time.time - startTime) * moveSpeed;
+            startPoint = transform;
+
+            fracJourney = distCovered / journeyLength;
+
+            newPosition = CalculateWorldspaceFromMouseClick();
+        }
+        //  transform.position = Vector3.Lerp(startPoint.position, newPosition, fracJourney);
+        transform.position = Vector3.SmoothDamp(startPoint.position, newPosition, ref velocity, smoothTime);
+    }
+
+    void MoveToPosition(float journeyLength)
+    {
+        Vector3 newPosition = CalculateWorldspaceFromMouseClick();
+
+        //  Calculate the values necessary for a Lerp
+        float distCovered = (Time.time - startTime) * moveSpeed;
+        print(Time.time);
+        print(startTime);
+        print(moveSpeed);
+        float fracJourney = distCovered / journeyLength;
+        Transform startPoint = transform;
+
+        //  Move to the new screen position
+        //do
+        {
+            transform.position = Vector3.Lerp(startPoint.position, newPosition, fracJourney);
+        } //while (transform.position != newPosition);
+    }
+
+    private static Vector3 CalculateWorldspaceFromMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        //  Get the camera information
+        Camera localCamera;
+        localCamera = FindObjectOfType<Camera>();
+
+        //  Calculate the a screen position based on the the worldspace information from the mouse click
+        Vector3 mouseSpacePos = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector3 newPosition = localCamera.ScreenToWorldPoint(mouseSpacePos);
+
+        return newPosition;
+    }
+}
---
+++ Ludum Dare 37/Assets/Dirt/Dirt.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dirt : MonoBehaviour {
+
+    public Sprite[] dirtSprites;
+
+    private Collider2D dirtCollider;
+    private SpriteRenderer dirtSpriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        dirtCollider = GetComponent<Collider2D>();
+        dirtSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetBallColour();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        Debug.Log(otherCollider.collider.name);
+
+        //  Switch the current sprite based on what is being collided with
+        SwapSprites(otherCollider);
+    }
+
+    private void SwapSprites(Collision2D otherCollider)
+    {
+        if (otherCollider.collider.name == "Cleaner")
+        {
+            SetBallColour();
+            //dirtSpriteRenderer.sprite = dirtSprites[1];
+        }
+        else if (otherCollider.collider.tag == "Dirt")
+        {
+            if (otherCollider.collider.GetComponent<SpriteRenderer>().sprite == dirtSprites[0])
+                SetBallColour();
+                //dirtSpriteRenderer.sprite = dirtSprites[0];
+        }
+    }
+
+    private void SetBallColour()
+    {
+        int spriteIndex = Random.Range(0, dirtSprites.Length);
+        dirtSpriteRenderer.sprite = dirtSprites[spriteIndex];
+    }
+}
