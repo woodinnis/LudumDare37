cb1695bea15f16e1411d59ce5c1a496e36a5481a
woodinnis <wood.innis@gmail.com>
2016-12-11 13:03:38 -0600

    Camera Rotation
    Camera rotation controls implemented using WASD/Arrow keys

---
+++ Ludum Dare 37/Assets/BallPit3D/Camera/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+    public float rotationSpeed;
+
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //  Camera Rotation controls
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.RotateAround(player.transform.position, Vector3.down, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.RotateAround(player.transform.position, Vector3.left, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.RotateAround(player.transform.position, Vector3.right, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
