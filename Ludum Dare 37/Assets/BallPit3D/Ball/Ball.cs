853b574b5ce97012f0278af5a331ea7894996a2f
woodinnis <wood.innis@gmail.com>
2016-12-11 11:40:41 -0600

    Replaced balls
    Balls in pit have been replaced with prefabs, balls now change colour
    when the game starts.

---
+++ Ludum Dare 37/Assets/BallPit3D/Ball/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Material[] ballMaterials;
+
+    private Collider ballCollider;
+    private MeshRenderer ballMeshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        ballCollider = GetComponent<Collider>();
+        ballMeshRenderer = GetComponent<MeshRenderer>();
+
+        SetBallColour();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SetBallColour()
+    {
+        int index = Random.Range(0, ballMaterials.Length);
+        ballMeshRenderer.material = ballMaterials[index];
+    }
+}
