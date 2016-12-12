4832aaa38622bb23609dad94a33d4de8deb03679
woodinnis <wood.innis@gmail.com>
2016-12-12 14:29:05 -0600

    New third person movement
    3rd person movement has been moved to the WASD/arrow keys

---
+++ Ludum Dare 37/Assets/BallPit3D/Player/gameMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameMovement : MonoBehaviour {
+
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Update is called once per frame
+    void Update () {
+
+        CharacterController controller = GetComponent<CharacterController>();
+        /*
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+        */
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+        
+    }
+}
