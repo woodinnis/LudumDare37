ba447cfe9270c6b2ecd3de2e4aa9a8327c6bdf44
woodinnis <wood.innis@gmail.com>
2016-12-11 19:15:51 -0600

    Camera Zoom added
    Player can zoom in and out using the scroll wheel

--- Ludum Dare 37/Assets/BallPit3D/Camera/GameCamera.cs
+++ Ludum Dare 37/Assets/BallPit3D/Camera/GameCamera.cs
@@ -4,12 +4,25 @@ using System.Collections;
 public class GameCamera : MonoBehaviour {
 
     public float rotationSpeed;
+    public float maxOut;
+    public float maxIn;
 
     private Player player;
+    private Camera thisCamera;
+
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
+
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<Player>();
-	}
+
+        thisCamera = GetComponent<Camera>();
+
+        //  Store the initial position and rotation of the camera at the start of the game
+        startingPosition = transform.position;
+        startingRotation = transform.rotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,6 +43,25 @@ public class GameCamera : MonoBehaviour {
         if (Input.GetAxis("Vertical") > 0)
         {
             transform.RotateAround(player.transform.position, Vector3.right, rotationSpeed * Time.deltaTime);
+        }
+
+        //  Camera Zoom controls
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(thisCamera.fieldOfView > maxIn)
+                thisCamera.fieldOfView--;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (thisCamera.fieldOfView < maxOut)
+                thisCamera.fieldOfView++;
+        }
+
+        //  Reset camera position and roation by pressing spacebar
+        if (Input.GetAxis("Jump") > 0)
+        {
+            transform.position = startingPosition;
+            transform.rotation = startingRotation;
         }
     }
 }
