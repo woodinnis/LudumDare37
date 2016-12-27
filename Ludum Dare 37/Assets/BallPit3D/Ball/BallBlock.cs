ac294fd8b697c442c71ac80b8ebff88d4099d49f
woodinnis <wood.innis@gmail.com>
2016-12-27 13:17:26 -0600

    Ball Block Changes
    Ball block Dimentions can now be set independantly
    Ball instantion now begins at the x,y,z of the Ball Block prefab
    
    A Total Ball Count has been added to the inspector

--- Ludum Dare 37/Assets/BallPit3D/Ball/BallBlock.cs
+++ Ludum Dare 37/Assets/BallPit3D/Ball/BallBlock.cs
@@ -3,35 +3,59 @@ using System.Collections;
 
 public class BallBlock : Ball {
 
-    public int ballsPerRow;
-    public float blockStartingX;
-    public float blockStartingY;
+    public int ballRowsLong;
+    public int ballRowsWide;
+    public int ballRowsHigh;
+
     public float spreadBallsApart;
     public GameObject ballToClone;
 
-    private int p_ballsPerRow;
+    private int p_ballRowsLong;
+    private int p_ballRowsWide;
+    private int p_ballRowsHigh;
     private float p_ballsSpawnSpread;
-    private float p_startingX;
-    private float p_startingY;
+
+    [SerializeField]
+    private int totalBallCount;
+
+    private GameObject[] getCount;
 
 	// Use this for initialization
 	void Start () {
-        p_ballsPerRow = ballsPerRow;
+        p_ballRowsLong = ballRowsLong;  //  X Axis
+        p_ballRowsHigh = ballRowsHigh;  //  Y Axis
+        p_ballRowsWide = ballRowsWide;  //  Z Axis
         p_ballsSpawnSpread = spreadBallsApart * 2;
 
+        totalBallCount = 0;
+
+        //  Establish the starting coordinates of the block
+        float myX = transform.position.x;
+        float myY = transform.position.y;
+        float myZ = transform.position.z;
+
         //  Create a new block of balls
-        for(int i = 0; i < p_ballsPerRow; i++)
+        //  X Axis
+        for (int i = 0; i < p_ballRowsLong; i++)
         {
-            for(int j = 0; j < p_ballsPerRow; j++)
+            //  Y Axis
+            for(int j = 0; j < p_ballRowsHigh; j++)
             {
-                for(int k = 0; k < p_ballsPerRow; k++)
+                //  Z Axis
+                for(int k = 0; k < p_ballRowsLong; k++)
                 {
                     float radius = ballToClone.GetComponent<SphereCollider>().radius * p_ballsSpawnSpread;
-
-                    Instantiate(ballToClone, new Vector3(i * radius, j * radius, k * radius), Quaternion.identity);
+                    
+                    Instantiate(ballToClone, new Vector3(myX + (i * radius), myY + ( j * radius), myZ + ( k * radius)), Quaternion.identity);
                 }
             }
         }
         
 	}
+
+    void Update()
+    {
+        getCount = GameObject.FindGameObjectsWithTag("Ball");
+        totalBallCount = getCount.Length;
+    }
 }
